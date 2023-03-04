6c7d9bc37d2ec42d3a3757a7e13fd3713c26e666
NoamBechhofer <noam.bechhofer@gmail.com>
2023-03-04 21:52:23 +1300

    Test class

---
+++ src/test/java/com/noambechhofer/yaniv/GameTest.java
@@ -0,0 +1,101 @@
+package com.noambechhofer.yaniv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameTest {
+    public static void main(String[] args) {
+        GameTest gt = new GameTest();
+        gt.testValidateSet();
+    }
+
+    
+    @Test
+    public void testValidateSet() {
+
+        Card[] allTwos = { new Card(Rank.TWO, Suit.CLUBS), new Card(Rank.TWO, Suit.DIAMONDS),
+                new Card(Rank.TWO, Suit.HEARTS), new Card(Rank.TWO, Suit.SPADES) };
+        Card redJoker = new Card(Rank.JOKER, Suit.HEARTS);
+        Card blackJoker = new Card(Rank.JOKER, Suit.SPADES);
+
+        Set<Card> cards = new HashSet<>();
+        for (int i = 0; i < 4; i++) {
+            cards.add(allTwos[i]);
+            assertTrue(Game.validate(cards));
+        }
+
+        cards = new HashSet<>();
+
+        cards.add(redJoker);
+        for (int i = 0; i < 4; i++) {
+            cards.add(allTwos[i]);
+            assertTrue(Game.validate(cards));
+        }
+
+        cards = new HashSet<>();
+
+        cards.add(redJoker);
+        cards.add(blackJoker);
+        for (int i = 0; i < 4; i++) {
+            cards.add(allTwos[i]);
+            assertTrue(Game.validate(cards));
+        }
+
+        cards = new HashSet<>();
+
+        cards.add(new Card(Rank.TWO, Suit.CLUBS));
+        cards.add(new Card(Rank.THREE, Suit.CLUBS));
+
+        assertFalse(Game.validate(cards));
+
+        cards = new HashSet<>();
+
+        for (Rank fv : Rank.values()) {
+            if (fv == Rank.JOKER)
+                continue;
+            Card tmp = new Card(fv, Suit.CLUBS);
+            cards.add(tmp);
+        }
+        assertTrue(Game.validate(cards));
+        cards.add(redJoker);
+        cards.add(blackJoker);
+        assertTrue(Game.validate(cards));
+
+        cards.remove(redJoker);
+        cards.remove(blackJoker);
+
+        for (Rank r : Rank.values()) {
+            if (r == Rank.JOKER)
+                continue;
+            Card c = new Card(r, Suit.CLUBS);
+
+            cards.remove(c);
+            cards.add(redJoker);
+
+            assertTrue(Game.validate(cards));
+
+            for (Rank r1 : Rank.values()) {
+                if (r1 == r || r1 == Rank.JOKER)
+                    continue;
+                Card c1 = new Card(r1, Suit.CLUBS);
+
+                cards.remove(c1);
+                cards.add(blackJoker);
+
+                assertTrue(Game.validate(cards));
+
+                cards.remove(blackJoker);
+                cards.add(c1);
+            }
+
+            cards.remove(redJoker);
+            cards.add(c);
+        }
+
+    }
+}

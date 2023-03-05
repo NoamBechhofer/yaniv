0482419c6b43fdbaf84d4ab9d7bde4bb5adfcf42
NoamBechhofer <noam.bechhofer@gmail.com>
2023-03-05 13:22:46 +1300

    implemented play()

--- src/main/java/com/noambechhofer/yaniv/Game.java
+++ src/main/java/com/noambechhofer/yaniv/Game.java
@@ -1,29 +1,113 @@
 package com.noambechhofer.yaniv;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
+// TODO: rename Dealer
 public class Game {
 
-    // we register players by giving them their starting hand
-    private Set<Player> players;
+    /** players in the game. */
+    private List<Player> players;
+    /** single deck of cards */
     private Deck deck;
-    private Stack<Set<Card>> discardPile; // Each discard is a set. That way we can access any of the cards from the
-                                          // previous discard.
-                                          
+    // Each discard is a set. That way we can access any of the cards from the
+    // previous discard.
+    private Stack<Set<Card>> discardPile;
+
+    private boolean yaniv;
     private boolean canSlap;
 
     public Game() {
-        this.players = new LockableHashSet<>();
+        this.players = new LinkedList<>();
 
         this.deck = new Deck();
         this.deck.shuffle();
 
         this.discardPile = new Stack<>();
+
+        this.yaniv = false;
+    }
+
+    // TODO: add method for player to quit
+
+    /**
+     * Play a game of Yaniv.
+     * 
+     * @param numPlayers the number of players in the game
+     */
+    public void play(int numPlayers) {
+        if (numPlayers < YanivProperties.MIN_PLAYERS || numPlayers > YanivProperties.MAX_PLAYERS)
+            throw new RuntimeException("invalid number of players " + numPlayers + ". Must be >= "
+                    + YanivProperties.MIN_PLAYERS + " and <= " + YanivProperties.MAX_PLAYERS);
+
+        // TODO: replace with listener
+        while (this.players.size() < numPlayers)
+            ;
+
+        while (players.size() > 1) {
+            this.deck.shuffle();
+
+            for (int i = 0; i < YanivProperties.NUM_STARTING_CARDS; i++)
+                for (Player p : players)
+                    p.dealToHand(this.deck.removeTopCard());
+
+            Iterator<Player> itr = this.players.iterator();
+            Player curr = null;
+
+            while (!this.yaniv) {
+                if (!itr.hasNext())
+                    itr = this.players.iterator();
+
+                this.canSlap = true;
+                curr = itr.next();
+                curr.doTurn(); // ! side effect: doTurn() sets canSlap to false. This is accomplished within
+                               // ! drawFromDiscard() and drawFromDeck(), which are the only two options for a
+                               // ! Player to call within their doTurn().
+            }
+            this.yaniv = false;
+            for (Player p : players)
+                if (p != curr)
+                    p.endRound();
+
+            for (Player p : players)
+                if (p.points() > YanivProperties.LOSING_THRESHOLD) {
+                    Player next = null;
+                    if (p == curr)
+                        if (itr.hasNext())
+                            next = itr.next();
+                        else
+                            next = players.get(0);
+                    else
+                        next = curr;
+                    players.remove(p);
+
+                    // We've modified players so we need a new iterator
+                    itr = players.iterator();
+                    while (next != itr.next())
+                        ;
+                }
+        }
+
+    }
+
+    /**
+     * Call Yaniv. The calling Player must be sure to tally their points properly
+     * after making this call, similar to as if their {@link Player#endRound()}
+     * method had been called.
+     * 
+     * @param hand the calling Player's hand
+     * @return true if the call is successful, false if the player is "assaf"ed
+     */
+    public boolean callYaniv(Collection<Card> hand) {
+        this.yaniv = true;
+        for (Player p : this.players)
+            if (tally(p.peekHand()) <= tally(hand))
+                return false;
+        return true;
     }
 
     /**
@@ -53,7 +137,7 @@ public class Game {
     public Set<Card> peekDiscardPile() {
         return this.discardPile.peek();
     }
-    
+
     /**
      * Draw from the discard pile.
      * 

5c518726c2911ade2f2fe7b7d50ee1f459ee7d70
NoamBechhofer <noam.bechhofer@gmail.com>
2023-06-21 00:08:50 +1200

    moved other top level classes to their own files

--- src/main/java/com/noambechhofer/yaniv/Card.java
+++ src/main/java/com/noambechhofer/yaniv/Card.java
@@ -1,6 +1,5 @@
 package com.noambechhofer.yaniv;
 
-import java.awt.Color;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
@@ -344,78 +343,5 @@ public class Card implements Comparable<Card> {
         }
 
         return true;
-    }
-}
-
-/**
- * The suit of a Card
- */
-enum Suit {
-    CLUBS, DIAMONDS, HEARTS, SPADES;
-
-    /**
-     * Returns {@link Color#BLACK} or {@link Color#RED}
-     * 
-     * @return {@link Color#BLACK} or {@link Color#RED}
-     */
-    public Color color() {
-        if (this == CLUBS || this == SPADES)
-            return Color.BLACK;
-        else
-            return Color.RED;
-    }
-
-    /**
-     * Returns a numerical representation of this Suit. CLUBS are 1, DIAMONDS are 2,
-     * HEARTS are 3, and SPADES are 4. Both jokers are 0.
-     * 
-     * @return a numerical representation of this Suit
-     */
-    public int asInt() {
-
-        switch (this) {
-            case CLUBS:
-                return 1;
-            case DIAMONDS:
-                return 2;
-            case HEARTS:
-                return 3;
-            case SPADES:
-                return 4;
-            default:
-                assert false : this;
-                return 0;
-        }
-    }
-}
-
-/** The rank of a Card */
-enum Rank {
-    ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8),
-    NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), JOKER(14);
-
-    /**
-     * numerical representation of this Rank. Useful for determining straights.
-     */
-    private int value;
-
-    Rank(int rank) {
-        this.value = rank;
-    }
-
-    /**
-     * Returns a numerical representation of this Rank. ACE is
-     * 1, TWO - TEN are 2 - 10, and JACK - KING are 11 - 13, JOKER is 14.
-     * 
-     * @return a numerical representation of this Rank
-     */
-    public int asInt() {
-        return this.value;
-    }
-}
-
-class CardConstructionException extends RuntimeException {
-    public CardConstructionException(String string) {
-        super(string);
     }
 }
